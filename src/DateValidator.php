945b02f8085b226f3b81d46c83532af541ade5c0
Dazza <adaza90@outlook.com>
2025-10-03 22:23:29 -0500

    Improvements

---
+++ src/DateValidator.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace DazzaDev\SriXmlGenerator;
+
+use DateTime;
+use Exception;
+
+class DateValidator
+{
+    /**
+     * Validate if a string is a valid date in Y-m-d format.
+     */
+    public function isValidDateFormat(string $date): bool
+    {
+        if (! preg_match('/^\d{4}-\d{2}-\d{2}$/', $date)) {
+            return false;
+        }
+
+        $parts = explode('-', $date);
+
+        return checkdate((int) $parts[1], (int) $parts[2], (int) $parts[0]);
+    }
+
+    /**
+     * Validate or convert the date to the America/Bogota timezone.
+     * Date must be in ISO 8601 format.
+     *
+     * @throws Exception If date is not in ISO 8601 format
+     */
+    public function validate(string|DateTime $date): DateTime
+    {
+        if ($date instanceof DateTime) {
+            return $date;
+        }
+
+        // Validate ISO 8601 format
+        if (! preg_match('/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:Z|[+-]\d{2}:?\d{2})?$/', $date)) {
+            throw new Exception('Date must be in ISO 8601 format (e.g. 2025-12-31T23:59:59Z) but got: ' . $date);
+        }
+
+        return new DateTime($date);
+    }
+
+    /**
+     * Get date in Y-m-d format
+     */
+    public function getDate(string|DateTime $date): string
+    {
+        $dateObject = $this->validate($date);
+
+        return $dateObject->format('Y-m-d');
+    }
+
+    /**
+     * Get time in H:i:s%z format
+     */
+    public function getTime(string|DateTime $date): string
+    {
+        $dateObject = $this->validate($date);
+
+        return $dateObject->format('H:i:sP');
+    }
+
+    /**
+     * Get DateTime object
+     */
+    public function getDateTime(string|DateTime $date): string
+    {
+        return $this->validate($date)->format('Y-m-d H:i:sP');
+    }
+}
